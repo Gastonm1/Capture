4dd74046768ad97069a94c0cb34cf22b79e643a1
Gastonm1 <gaston.marques@gmail.com>
2022-08-09 16:50:44 -0500

    Contact creation and animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,77 @@
 import React from "react";
 //Framer Motion
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnimation } from "../animation";
+
+//Styles
+import styled from "styled-components";
 
 const ContactUS = () => {
   return (
-    <motion.div
+    <ContactStyle
       exit="exit"
       variants={pageAnimation}
       initial="hidden"
       animate="show"
-      style={{background: "#fff"}}
+      style={{ background: "#fff" }}
     >
-      <h1>Contact</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}>Get In Touch</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Send An Email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Contact</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Socials</h2>
+            {/* <a href="www.twitter.com">Twitter</a> */}
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUS;
